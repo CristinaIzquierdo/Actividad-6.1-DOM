2e2d7bc53e94320d79838c9f7dc2c4aa7cd9ddb2
Cristinaizquierdo2020 <cristinaizquierdo2000@gmail.com>
2021-12-17 14:08:01 +0100

    Se añade la lista vacia

--- scripts/u6e2_lista.js
+++ scripts/u6e2_lista.js
@@ -1,1 +1,19 @@
-function addNewList() {}
+document.addEventListener('DOMContentLoaded', iniciar);
+
+function iniciar() {
+  document.getElementById('addLista').addEventListener('click', addNewList);
+}
+function addNewList() {
+  const main = document.getElementsByTagName('main')[0];
+  const section = document.createElement('section');
+  const unorderedList = document.createElement('ul');
+  const btn = document.createElement('button');
+  btn.textContent = 'Añadir elemento';
+  //TODO: quitar linea (section with red borders)
+  section.style.border = '1px solid red';
+  main.appendChild(section);
+  section.appendChild(unorderedList);
+  //TODO: terminar
+  const listName = prompt('Indica el nombre de la lista');
+  unorderedList.appendChild(btn);
+}
